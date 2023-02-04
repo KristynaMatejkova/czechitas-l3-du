92cacf4d1797933b9b7a7c55924428fa48d64d0a
LAPTOP-6A2IC92J\rdhd1 <junova.kristyna@gmail.com>
2023-02-04 18:20:38 +0100

    Změna výstupu

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 // tady je místo pro náš program
 
+let jmenoUzivatele = prompt("Zadejte své jméno a příjmení:");
+let rokNarozeni = Number(prompt("Zadejte svůj rok narození:"));
 
+function vek() {
+    let tentoRok = new Date().getFullYear();
+    let vekUzivatele = tentoRok - rokNarozeni;
+    return vekUzivatele;
+}
 
+let vystup = document.querySelector(".vystupProgramu");
+vystup.innerHTML = jmenoUzivatele + "; " + vek() + " let";
 
+let oblibenaBarva = prompt("Zadejte ANIGLICKÝ název své oblíbené barvy:")
+vystup.style.color = oblibenaBarva;
